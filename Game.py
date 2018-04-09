c1a1a0db6cd7cfffec1e6b007269ec12bddcd263
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2018-04-09 15:30:21 -0300

    Documentation

--- Game.py
+++ Game.py
@@ -1,13 +1,15 @@
 from utils import randomPlace
 from texttable import Texttable
 
+# CONSTANTS
 PIT = "PIT"
 PLAYER = "PLAYER"
 BREEZE = "BREEZE"
 STENCH = "STENCH"
 WUMPUS = "WUMPUS"
+GOLD = "GOLD"
 
-
+# GAME
 class Game:
     def __init__(self, size):
         self._size = size
@@ -28,12 +30,15 @@ class Game:
 
         self.generateWorld()
 
-    def placeInBoard(self, lin, col, content):
-        self._board[lin][col].append(content)
-
+    # INITIALIZE MATRIX
     def initializeBoard(self):
         return [[[] for x in range(self._size)] for lin in range(self._size)]
 
+    # PLACE A CONSTANT ON THE BOARD
+    def placeInBoard(self, lin, col, content):
+        self._board[lin][col].append(content)
+
+    # PRINT BOARD
     def printBoard(self):
         row = []
         for lin in self._board:
@@ -45,16 +50,16 @@ class Game:
         t.add_rows(row)
         print(t.draw())
 
+    # CHECK IF GIVEN PLACE CONTAINS PARAMETER
     def checkPosition(self, lin, col, content):
         return content in self._board[lin][col]
 
-    def isEmpty(self, lin, col):
-        return len(self._board[lin][col]) == 0
-
+    # PLACE A PIT IN THE BOARD
     def placePit(self, pos):
         self.placeInBoard(pos[0], pos[1], PIT)
         self._pits.append(pos)
 
+    # LOOP OVER THE PITS AND PLACES ALL THE BREEZES
     def putBreezes(self):
         for pit in self._pits:
             pLin = pit[0]
@@ -85,6 +90,7 @@ class Game:
                     if(not(self.checkPosition(pLin, pCol - 1, BREEZE))):
                         self.placeInBoard(pLin, pCol - 1, BREEZE)
 
+    # PLACE WUMPUS AND CHECK AND PUT STENCHS
     def placeWumpus(self, pos):
         self.placeInBoard(pos[0], pos[1], WUMPUS)
 
@@ -116,6 +122,11 @@ class Game:
                 if(not(self.checkPosition(pLin, pCol - 1, STENCH))):
                     self.placeInBoard(pLin, pCol - 1, STENCH)
 
+    # PLACE GOLD IN THE BOARD
+    def placeGold(self, goldPos):
+        self.placeInBoard(goldPos[0], goldPos[1], GOLD)
+
+    # GENERATE WORLD
     def generateWorld(self):
         # GENERATE PITS AND BREEZES
         for i in range(self._nPits):
@@ -132,4 +143,11 @@ class Game:
             wumpusRandomPosition = randomPlace(self._size)
             if(not(wumpusRandomPosition == self._player) and wumpusRandomPosition not in self._pits):
                 self.placeWumpus(wumpusRandomPosition)
+                break
+
+        # GENERATE GOLD
+        while(True):
+            goldRandomPosition = randomPlace(self._size)
+            if(not(goldRandomPosition == self._player) and (goldRandomPosition not in self._pits)):
+                self.placeGold(goldRandomPosition)
                 break
